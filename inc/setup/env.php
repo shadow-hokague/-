5e65e3d91f90d27b564c70c2bbb6dd620f45ef7e
shadow-hokague <duboisclement853@gmail.com>
2025-06-03 17:06:33 +0200

    Update env.php

--- inc/setup/env.php
+++ inc/setup/env.php
@@ -2,11 +2,11 @@
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 
 if (!defined('DB_DATABASE')) define('DB_DATABASE', 'suplike');
-if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
-if (!defined('DB_USERNAME')) define('DB_USERNAME', 'root');
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '');
+if (!defined('DB_HOST')) define('DB_HOST', 'mysql-alaye.alwaysdata.net');
+if (!defined('DB_USERNAME')) define('DB_USERNAME', 'alaye');
+if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '@Motdepasse0000');
 if (!defined('DB_PORT')) define('DB_PORT', 3306);
-if (!defined('APP_NAME')) define('APP_NAME', 'Suplike social network');
+if (!defined('APP_NAME')) define('APP_NAME', 'ΛⱠΛɎɆ social network');
 if (!defined('SETUP')) define('SETUP', false);
 if (!defined('BASE_URL')) define('BASE_URL', $protocol . $_SERVER['HTTP_HOST']);
 if (!defined('EMAIL_VERIFICATION')) define('EMAIL_VERIFICATION', false);
